34c4ba6e3a2ca9384e98a8968252da34f92658d4
Vinermy <egorkosachev@vk.com>
2025-04-18 18:40:13 +0300

    fixed bugs + added registration view

---
+++ intellectum_app/forms.py
@@ -0,0 +1,41 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegistrationForm(UserCreationForm):
+    password1 = forms.CharField(
+        label="Пароль",
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        help_text="Минимум 8 символов. Минимум 1 буква или специальный символ",
+    )
+    password2 = forms.CharField(
+        label="Подтверждение пароля",
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
+        help_text="Совпадает с паролем",
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "email",
+        )
+        labels = {
+            "first_name": "Имя",
+            "last_name": "Фамилия",
+            "email": "E-mail",
+        }
+        help_texts = {
+            "first_name": "",
+            "last_name": "",
+            "email": "Должен быть действительным адресом электронной почты",
+        }
+
+
+class SigninForm(forms.Form):
+    username = forms.CharField(label="Логин")
+    password = forms.CharField(widget=forms.PasswordInput, label="Пароль")
--- intellectum_app/views.py
+++ intellectum_app/views.py
@@ -1,11 +1,23 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
+from intellectum_app.forms import RegistrationForm
 # Create your views here.
 
 
-def home_view(request: HttpRequest):
+def home_view(request):
     return render(request, "auth/registration.html")
 
-def auth_registration(request: HttpRequest):
-    
+
+def registration_view(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_staff = True
+            user.save()
+            login(request, user, backend="axes.backends.AxesBackend")
+            return redirect("home")
+    else:
+        form = RegistrationForm()
+    return render(request, "auth/registration.html", {"form": form})
