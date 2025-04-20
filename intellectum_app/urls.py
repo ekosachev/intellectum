34bb3bfd5eca2cfe3d912230d71935226476d0c9
Vinermy <egorkosachev@vk.com>
2025-04-21 00:57:14 +0300

    fixed auth views

--- intellectum_app/urls.py
+++ intellectum_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path("courses", courses_view, name="courses"),
     path("assignments", assignments_view, name="assignments"),
     path("auth/login/", auth_views.LoginView.as_view(template_name="auth/login.html"), name="login"),
-    path("auth/register/", registration_view),
+    path("auth/register/", registration_view, name="register"),
     path("logout", logout_view, name="logout_view"),
     path("widget/profile", profile_widget),
     path("widget/upcoming_lessons", upcoming_lessons),
